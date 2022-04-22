0409924c7a778845cd6c5616cca2fd27c2a85c14
Mitlasóczki Bence <mitlabence111@gmail.com>
2022-04-22 13:23:13 +0200

    Added function for assisting integration of Matlab functions in Python

---
+++ setOptionsForPython.m
@@ -0,0 +1,7 @@
+function options = setOptionsForPython(filename)
+%SETOPTIONSFORPYTHON This function makes it easy in Python to prepare a
+% struct options for nd2ReadWithOptions.
+%   Detailed explanation goes here
+options.filename = filename;
+end
+
